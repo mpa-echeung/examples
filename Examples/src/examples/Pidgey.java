c0bca11b17efff1656bb18dad7e1a542b70c750c
echeung <echeung@Classroom18.metro.internal>
2016-04-20 10:43:25 -0400

    Pidgey is commented

--- Examples/src/examples/Pidgey.java
+++ Examples/src/examples/Pidgey.java
@@ -4,7 +4,8 @@ class Pidgey extends Pokemon {
 
     public Pidgey(int attack, int defense, int level){
         super(attack, defense, level, "Flying");
-        this.name = "Pidgey";
+        this.name = "Pidgey"; 
+        //This is a pidgey
     }
     
 }
