353a6983f1937b9001a7fe0cf74441712bf8a762
echeung <echeung@Classroom18.metro.internal>
2016-04-20 10:58:39 -0400

    I added a comment about the arena

--- Examples/src/examples/Examples.java
+++ Examples/src/examples/Examples.java
@@ -3,6 +3,7 @@ package examples;
 public class Examples {
 
     public static void main(String[] args) {
+        //This is the main arena
         Pokemon poke1= new Pidgey(5, 5, 10);
         Pokemon poke2= new Pidgey(2, 7, 10);
         Battlefield test = new Battlefield(poke1, poke2);
